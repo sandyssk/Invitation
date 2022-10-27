cc53f1a9823834fc2261d86ceed71f34eaaac34f
Sandeep Kumar <ssksandeepkumar18@gmail.com>
2022-10-27 16:45:01 +0530

    first commit

---
+++ weddingpro.js
@@ -0,0 +1,15 @@
+let name = document.getElementById("name")
+let error = document.getElementById("error")
+let invite_name = document.getElementById("invitenames")
+let section = document.getElementById("sectionhome")
+let button = document.getElementById("button")
+function startbutton(){
+    if (name.value==="") {
+        error.textContent = "Please Enter a Valid Input."
+    }
+    else{
+        error.textContent = "";
+        invite_name.textContent = name.value
+        button.onclick = ("section")
+    }
+}
